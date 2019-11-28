b9e1c507a0d3f389ad59d6b75cde30be68bc827e
Tomasz Osial <tomasz.osial@gmail.com>
2019-11-28 09:39:53 +0100

    Updates

---
+++ precache-manifest.47e634d619f84bc9b1097df770526de4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79494c7e95a590b4a10da746994892b",
+    "url": "/Birtday_App/index.html"
+  },
+  {
+    "revision": "428fa26015a3741979fd",
+    "url": "/Birtday_App/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "afda3c01c4e833e13b0a",
+    "url": "/Birtday_App/static/css/main.ad61c778.chunk.css"
+  },
+  {
+    "revision": "428fa26015a3741979fd",
+    "url": "/Birtday_App/static/js/2.3d97964b.chunk.js"
+  },
+  {
+    "revision": "afda3c01c4e833e13b0a",
+    "url": "/Birtday_App/static/js/main.74e4990d.chunk.js"
+  },
+  {
+    "revision": "11d5f74530314ca66ec1",
+    "url": "/Birtday_App/static/js/runtime-main.7131fe45.js"
+  },
+  {
+    "revision": "2697fce450593a5c8853ce19b48bef24",
+    "url": "/Birtday_App/static/media/4k-wallpaper-adorable-blur-boy-1148998.2697fce4.jpg"
+  },
+  {
+    "revision": "c7c35c89a8b43b537db79b80da793487",
+    "url": "/Birtday_App/static/media/action-activity-balls-day-296302.c7c35c89.jpg"
+  },
+  {
+    "revision": "e32548c09c2a7d7fad7b5c311ce836ca",
+    "url": "/Birtday_App/static/media/action-activity-boy-children-296301.e32548c0.jpg"
+  },
+  {
+    "revision": "77cf53156ced55d0537c4d80238c30bc",
+    "url": "/Birtday_App/static/media/boy-playing-with-fall-leaves-outdoors-36965.77cf5315.jpg"
+  },
+  {
+    "revision": "bb69b69ec4a4810d9cdd95698ba68e95",
+    "url": "/Birtday_App/static/media/child-clouds-enjoyment-environment-552168.bb69b69e.jpg"
+  },
+  {
+    "revision": "22c2277a529e5ffc9d5fe710336187f3",
+    "url": "/Birtday_App/static/media/depth-of-field-photography-of-p-l-a-y-wooden-letter-decors-591652.22c2277a.jpg"
+  },
+  {
+    "revision": "1f0cc65c04180bc847861853964e7931",
+    "url": "/Birtday_App/static/media/photo-of-children-playing-with-dry-leaves-1582736.1f0cc65c.jpg"
+  },
+  {
+    "revision": "7416ae939781a132fa82e6dbb4019921",
+    "url": "/Birtday_App/static/media/ramin-talebi-u_b0jWnkgM0-unsplash.7416ae93.jpg"
+  },
+  {
+    "revision": "8ee13c8f0f9b57d1530761ba2dfb0289",
+    "url": "/Birtday_App/static/media/sunglasses-girl-swimming-pool-swimming-61129.8ee13c8f.jpg"
+  }
+]);
