a8d284340227197796cd74be69cc189d2d54cdec
Tomasz Osial <tomasz.osial@gmail.com>
2019-11-29 10:24:15 +0100

    Finalne poprawki

--- src/components/Footer/Footer.stories.js
+++ src/components/Footer/Footer.stories.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Footer from './Footer';
 import {storiesOf} from "@storybook/react";
-import Header from "../Header/header";
 
 storiesOf('Footer', module)
     .add('default', () => <Footer />);
