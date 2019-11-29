e59a68bc8152634741c8e66827beab89aef44279
Tomasz Osial <tomasz.osial@gmail.com>
2019-11-29 10:25:10 +0100

    Updates

---
+++ precache-manifest.629f9e60ebf9b9108939952acbcfada9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7e11d9c8cf9a7cda0bff3af4a1b582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f5a6cd7c852ab2732ba",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "4764dcadb39333d794a9",
+    "url": "/static/css/main.eeef6e16.chunk.css"
+  },
+  {
+    "revision": "2f5a6cd7c852ab2732ba",
+    "url": "/static/js/2.8811b614.chunk.js"
+  },
+  {
+    "revision": "4764dcadb39333d794a9",
+    "url": "/static/js/main.1b9e0c02.chunk.js"
+  },
+  {
+    "revision": "452384709423df0dc023",
+    "url": "/static/js/runtime-main.e4695665.js"
+  },
+  {
+    "revision": "2697fce450593a5c8853ce19b48bef24",
+    "url": "/static/media/4k-wallpaper-adorable-blur-boy-1148998.2697fce4.jpg"
+  },
+  {
+    "revision": "c7c35c89a8b43b537db79b80da793487",
+    "url": "/static/media/action-activity-balls-day-296302.c7c35c89.jpg"
+  },
+  {
+    "revision": "e32548c09c2a7d7fad7b5c311ce836ca",
+    "url": "/static/media/action-activity-boy-children-296301.e32548c0.jpg"
+  },
+  {
+    "revision": "77cf53156ced55d0537c4d80238c30bc",
+    "url": "/static/media/boy-playing-with-fall-leaves-outdoors-36965.77cf5315.jpg"
+  },
+  {
+    "revision": "bb69b69ec4a4810d9cdd95698ba68e95",
+    "url": "/static/media/child-clouds-enjoyment-environment-552168.bb69b69e.jpg"
+  },
+  {
+    "revision": "22c2277a529e5ffc9d5fe710336187f3",
+    "url": "/static/media/depth-of-field-photography-of-p-l-a-y-wooden-letter-decors-591652.22c2277a.jpg"
+  },
+  {
+    "revision": "1f0cc65c04180bc847861853964e7931",
+    "url": "/static/media/photo-of-children-playing-with-dry-leaves-1582736.1f0cc65c.jpg"
+  },
+  {
+    "revision": "7416ae939781a132fa82e6dbb4019921",
+    "url": "/static/media/ramin-talebi-u_b0jWnkgM0-unsplash.7416ae93.jpg"
+  },
+  {
+    "revision": "8ee13c8f0f9b57d1530761ba2dfb0289",
+    "url": "/static/media/sunglasses-girl-swimming-pool-swimming-61129.8ee13c8f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Birtday_App/precache-manifest.1208bff91ffdad288ab2ba234e1a2684.js"
+  "/precache-manifest.629f9e60ebf9b9108939952acbcfada9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Birtday_App/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
